68529b4b7487dcc0e594f4e3d914f7c3cddf7862
16007103 <16007103@PC-004568.uhi.ad.local>
2019-04-30 09:41:58 +0100

    First Commit

---
+++ src/clock/QueueOverflowException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clock;
+
+/**
+ * Queue is full.
+ *
+ * Cannot accept a new item.
+ */
+public class QueueOverflowException extends Exception {
+
+    public QueueOverflowException() {
+        super("Queue is full");
+    }
+}
---
+++ src/clock/QueueUnderflowException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clock;
+
+/**
+ * Queue is empty.
+ *
+ * Cannot access or remove an item from an empty queue.
+ */
+public class QueueUnderflowException extends Exception {
+
+    public QueueUnderflowException() {
+        super("Queue is empty");
+    }
+}
